ff497d8f0756678eb43c057c75bb19a45867899e
scewpsdev <jpg4859@gmail.com>
2024-10-20 20:41:00 +0200

    .

---
+++ PixelEngine/publish.bat
@@ -0,0 +1,20 @@
+del /s /q bin\Release\net8.0\publish
+
+dotnet publish -r win-x64 -c Release --self-contained -o bin/Release/net8.0/publish
+del bin\Release\net8.0\publish\*.pdb
+
+del res\asset_table
+del ..\RainfallNative\res\asset_table
+del ..\Rainfall2D\res\asset_table
+
+cmd /C ..\RainfallResourceCompiler\bin\x64\Debug\RainfallResourceCompiler.exe res bin\Release\net8.0\publish\res png ogg vsh fsh csh ttf rfs
+cmd /C ..\RainfallResourceCompiler\bin\x64\Debug\RainfallResourceCompiler.exe ..\RainfallNative\res bin\Release\net8.0\publish\res png ogg vsh fsh csh ttf rfs
+cmd /C ..\RainfallResourceCompiler\bin\x64\Debug\RainfallResourceCompiler.exe ..\Rainfall2D\res bin\Release\net8.0\publish\res png ogg vsh fsh csh ttf rfs
+
+xcopy /y ..\RainfallNative\bin\x64\Release\RainfallNative.dll bin\Release\net8.0\publish\
+
+ren bin\Release\net8.0\publish\PixelEngine.exe IvoryKeep.exe
+
+echo cmd /k IvoryKeep.exe > bin\Release\net8.0\publish\launch.bat
+
+pause
