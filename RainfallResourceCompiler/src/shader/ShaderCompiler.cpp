f98b6033436ec522e758ea7cb6528ac83991dfd6
scewpsdev <mail.faris.m@gmail.com>
2026-03-14 11:18:05 +0100

    .

--- RainfallResourceCompiler/src/shader/ShaderCompiler.cpp
+++ RainfallResourceCompiler/src/shader/ShaderCompiler.cpp
@@ -195,7 +195,7 @@ bool CompileBGFXShader(const char* path, const char* out, const char* type)
 bool CompileSPIRVShader(const char* path, const char* out, const char* type)
 {
 	char cmd[256];
-	sprintf(cmd, "glslc -fshader-stage=%s -O %s -o %s", type, path, out);
+	sprintf(cmd, "glslc -fshader-stage=%s -O0 %s -o %s", type, path, out);
 	for (int i = 0; i < (int)strlen(cmd); i++)
 	{
 		if (cmd[i] == '\\')
